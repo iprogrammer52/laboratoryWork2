18b36d489d56542d7be52fd25b72c6755867c0a6
Ivan Balashov <iprogrammer52@gmail.com>
2020-01-24 07:49:20 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace laboratoryWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine("");
+        }
+    }
+}
